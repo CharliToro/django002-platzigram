4b87b90981123613ab7ee658149590957dad6a51
charli <carlostoro04@gmail.com>
2018-08-28 14:30:43 -0500

    Primer commit Proyecto platzigram class based views

---
+++ posts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from posts import views
+
+urlpatterns = [
+    path(
+        route='',
+        view=views.PostsFeedView.as_view(),
+        name='feed',
+    ),
+    path(
+        route='posts/new/',
+        view=views.create_post,
+        name='create_post',
+    ),
+]
